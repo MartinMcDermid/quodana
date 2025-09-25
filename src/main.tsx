e2951c944a7ceff17ae93ead12f567de69830b14
Itz-Mattmatt <31461563+Itz-Mattmatt@users.noreply.github.com>
2025-09-25 15:22:50 +0100

    test

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import App from "./App";
 import "./styles.css";
 
 const container = document.getElementById("root");
+
+const SECRET_PASSWORD: number = "1233445"
+
 if (!container) {
   throw new Error("Root container #root not found");
 }
